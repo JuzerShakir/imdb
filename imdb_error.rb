b583126a2253cf5ddc0c5397238b812385253bb1
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-02 14:54:07 +0530

    add comments to error classes

--- imdb_error.rb
+++ imdb_error.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
+# All custom errors will inherit from this class
 class ImdbError < StandardError; end
 
+# Will raise this error for incorrect url input
 class InvalidURL < ImdbError; end
 
+# Will raise this error if URL is NOT Movie type
 class MovieTypeError < InvalidURL; end
 
+# Will raise this error if URL is NOT TvShow type
 class TvShowTypeError < InvalidURL; end
 
+# Will raise this error if URL is NOT EpisodeType type
 class EpisodeTypeError < InvalidURL; end
 
+# Will raise this error if URL is NOT VideoGame type
 class VideoGameTypeError < InvalidURL; end
