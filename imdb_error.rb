fdbad5fc818b546ca549bbdd4a6495ac9be1503e
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-02 20:15:55 +0530

    remove no-longer-needed errors & add new error for unsupported title type

--- imdb_error.rb
+++ imdb_error.rb
@@ -6,14 +6,5 @@ class ImdbError < StandardError; end
 # Will raise this error for incorrect url input
 class InvalidURL < ImdbError; end
 
-# Will raise this error if URL is NOT Movie type
-class MovieTypeError < InvalidURL; end
-
-# Will raise this error if URL is NOT TvShow type
-class TvShowTypeError < InvalidURL; end
-
-# Will raise this error if URL is NOT EpisodeType type
-class EpisodeTypeError < InvalidURL; end
-
-# Will raise this error if URL is NOT VideoGame type
-class VideoGameTypeError < InvalidURL; end
+# Will raise this error if URL is UNSUPPORTED title type
+class UnSupportedTypeError < InvalidURL; end
