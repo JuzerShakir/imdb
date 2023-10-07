fd54be01c60f8c081d6350bca892a1f04d8ca1d6
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-07 19:05:45 +0530

    test inheritance for all media types

--- test/titles/tv_show_test.rb
+++ test/titles/tv_show_test.rb
@@ -5,6 +5,10 @@ require_relative "../../lib/imdb_title"
 
 # tests against all the methods of TvShow Module
 class TestTvShow < Minitest::Test
-  # breaking_bad = "https://www.imdb.com/title/tt0903747"
-  # TV = IMDb::Title.new(breaking_bad)
+  breaking_bad = "https://www.imdb.com/title/tt0903747"
+  TV = IMDb::Title.new(breaking_bad)
+
+  def test_inheritance
+    assert_kind_of(TvShow, TV)
+  end
 end
