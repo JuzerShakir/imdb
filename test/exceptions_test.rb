62e7d540c5d2495054b51303ef963b705d96cc38
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-27 17:26:22 +0530

    FIX `DuplicateMethodCall` offense

--- test/exceptions_test.rb
+++ test/exceptions_test.rb
@@ -7,7 +7,9 @@ require_relative "test_helper"
 class ExceptionsTest < Minitest::Test
   def test_invalid_url
     assert_raises(IMDb::InvalidURL) { IMDb::Title.new("http://www.example.com") }
+  end
 
+  def test_invalid_id
     VCR.use_cassette("wrong_id") do
       assert_raises(IMDb::InvalidURL) { IMDb::Title.new("https://www.imdb.com/title/tt549199490/") }
     end
