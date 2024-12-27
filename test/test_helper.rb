7f0270f05a5b4ce02a1d08b465d38f6d978f83e0
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-27 10:26:44 +0530

    Require `require_all`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 # load external libraries
+require "debug"
 require "minitest/autorun"
 require "minitest/hooks"
-require "debug"
+require 'require_all'
 
 require_relative "../lib/imdb_title"
 require_relative "imdb_title_test"
