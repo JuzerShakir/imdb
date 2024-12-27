b1b6c74f853e6fab9321addaa73c515ec0d96092
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-27 16:15:36 +0530

    FIX `Minitest/AssertRaisesCompoundBody` offense and use vcr for wrong id

--- test/exceptions_test.rb
+++ test/exceptions_test.rb
@@ -6,9 +6,10 @@ require_relative "test_helper"
 # tests for custom exceptions
 class ExceptionsTest < Minitest::Test
   def test_invalid_url
-    assert_raises(IMDb::InvalidURL) do
-      IMDb::Title.new("http://www.example.com")
-      IMDb::Title.new("https://www.imdb.com/title/tt549199490/")
+    assert_raises(IMDb::InvalidURL) { IMDb::Title.new("http://www.example.com") }
+
+    VCR.use_cassette("wrong_id") do
+      assert_raises(IMDb::InvalidURL) { IMDb::Title.new("https://www.imdb.com/title/tt549199490/") }
     end
   end
 end
