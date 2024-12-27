261bcea78f30a739d8a003941e98006ff46c36d8
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-27 09:57:29 +0530

    Require external libraries first

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+# load external libraries
 require "minitest/autorun"
 require "minitest/hooks"
+require "debug"
+
 require_relative "../lib/imdb_title"
 require_relative "imdb_title_test"
 require_relative "non_interactive_test"
-require "debug"
 
 # This helper class loads all the modules and methods required for the tests
 class TestHelper < Minitest::Test
